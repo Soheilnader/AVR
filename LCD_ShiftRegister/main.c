d98e4238ca72617f1ba58f1d309cd684d418aa59
Soheil <sohail_nader@yahoo.com>
2022-03-19 00:36:19 +0330

    Update main.c

--- LCD_ShiftRegister/main.c
+++ LCD_ShiftRegister/main.c
@@ -1,17 +1,39 @@
 
 #include <mega32.h>
+#include "lcd.h"
+#include <delay.h>
 
-// Declare your global variables here
 
 void main(void)
 {
 
-DDRC = 0xFF;
-PORTC = 0;
+DDRC=0xFF;
+DDRD=0xFF;
 
-while (1)
-      {
-      // Place your code here
+PORTC=0;
+PORTD=0;
 
-      }
+
+
+lcd_init();
+lcd_putchar('A');
+delay_ms(1000);
+lcd_clear();
+lcd_gotoxy(1,0);
+lcd_putchar('A');
+delay_ms(1000);
+lcd_clear();
+lcd_gotoxy(1,5);
+
+lcd_puts("salam");
+delay_ms(1000);
+lcd_clear();
+lcd_puts("salam Soheil");
+lcd_gotoxy(1,0);
+lcd_puts("Nadernezhad");
+
+
+    while(1){
+    
+    }
 }
